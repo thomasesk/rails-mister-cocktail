d5b64fd75dd97d2a390393262c055ac3db7f2f86
Thomas Eskenazi <thomas.eskenazi@hec.edu>
2019-03-01 17:53:44 +0100

    Ajout d'ingrédients au seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
 Ingredient.destroy_all
 
 Ingredient.create(name: "Lemon")
